32f1a6530d09f8491349bae3f8daec01deaf27e9
Graham Taylor <Graham@Grahams-MacBook-Pro.local>
2016-06-09 00:13:57 -0500

    task one complete

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 // GO!
 
 console.log('script loaded')
+
+
+//vars go up here
+var hideButton = document.querySelector("#hide-nav button")
+
+var navBar = document.querySelector("#hide-nav div.nav-menu")
+
+var hideNavStatus = {
+	hidden: false
+}
+
+var hideExample = function(){
+	// alert('test')	
+	if(hideNavStatus.hidden === false){
+		// alert('is hidden')
+		navBar.style.opacity = 0
+		// navBar.style.visibility = 'hidden'
+		hideNavStatus.hidden = true
+	} else {
+		// alert('is visible')
+		navBar.style.opacity = 1
+		// navBar.style.visibility = 'visible'
+		hideNavStatus.hidden = false
+	}
+}
+
+hideButton.addEventListener('click', hideExample)
+
